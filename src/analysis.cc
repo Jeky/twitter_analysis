3d07ad2ac8d482856ba661f78288ed5f95b650f4
Jeky <jeky.cui@gmail.com>
2015-08-26 16:13:21 -0400

    add convert function

--- src/analysis.cc
+++ src/analysis.cc
@@ -5,11 +5,14 @@ void convertToDS() {
     unordered_map<long, User> *spammers = loadSpammers();
     unordered_map<long, User> *nonSpammers = loadNonSpammers();
 
+    LOG("User loaded. Convert users to Dataset...");
+
     Dataset *spammerDS = user2Dataset(spammers, 1);
     delete spammers;
     Dataset *nonSpammerDS = user2Dataset(nonSpammers, 1);
     delete nonSpammers;
 
+    LOG("Save datasets...");
     saveObject(spammerDS, PATH + "spammer.dat");
     delete spammerDS;
     saveObject(nonSpammerDS, PATH + "nonspammer.dat");
@@ -21,6 +24,6 @@ void printDatasetStatistic(Dataset *spammer, Dataset *nonSpammer) {
 }
 
 int main(int argc, char const *argv[]) {
-//    convertToDS();
+    convertToDS();
     return 0;
 }
