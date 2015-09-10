e1fb3e35601208cd58c2e5662c4a3733b1bf5a66
Jeky <jeky.cui@gmail.com>
2015-09-10 16:26:34 -0400

    add counter

--- src/analysis.cc
+++ src/analysis.cc
@@ -8,235 +8,250 @@
 #include <iterator>
 
 void convertToDS() {
-    auto *spammers = loadSpammers();
-    auto *nonSpammers = loadSampledNonSpammers();
+	auto *spammers = loadSpammers();
+	auto *nonSpammers = loadSampledNonSpammers();
 
-    LOG("User loaded. Convert users to Dataset...");
+	LOG("User loaded. Convert users to Dataset...");
 
-    auto *spammerDS = user2Dataset(spammers);
-    delete spammers;
+	auto *spammerDS = user2Dataset(spammers);
+	delete spammers;
 
-    auto *nonSpammerDS = user2Dataset(nonSpammers);
-    delete nonSpammers;
+	auto *nonSpammerDS = user2Dataset(nonSpammers);
+	delete nonSpammers;
 
-    LOG("Save datasets...");
-    saveObject(spammerDS, SPAMMER_DS);
-    delete spammerDS;
+	LOG("Save datasets...");
+	saveObject(spammerDS, SPAMMER_DS);
 
-    saveObject(nonSpammerDS, NON_SPAMMER_DS);
-    delete nonSpammerDS;
+	delete spammerDS;
+
+	saveObject(nonSpammerDS, NON_SPAMMER_DS);
+	delete nonSpammerDS;
 }
 
 Counter<string> *countTokens(unordered_map<long, User> *users,
-                             const string &path) {
-    ifstream infile(path);
+		const string &path) {
+	ifstream infile(path);
 
-    if (!infile.good()) {
-        LOG("Cannot Find ", path, ". Counting Tokens...");
-        infile.close();
+	if (!infile.good()) {
+		LOG("Cannot Find ", path, ". Counting Tokens...");
+		infile.close();
 
-        Counter<string> *tokenCounter = new Counter<string>();
-        int i = 0;
-        for (auto &kv : *users) {
-            if (i % 1000 == 0) {
-                LOG("Process ", i, " users");
-            }
-            for (auto &t : kv.second.getTweets()) {
-                vector<string> *words = toGrams(t.getText());
-                tokenCounter->count(words);
-                delete words;
-            }
-            i++;
-        }
+		Counter < string > *tokenCounter = new Counter<string>();
+		int i = 0;
+		for (auto &kv : *users) {
+			if (i % 1000 == 0) {
+				LOG("Process ", i, " users");
+			}
+			for (auto &t : kv.second.getTweets()) {
+				vector < string > *words = toGrams(t.getText());
+				tokenCounter->count(words);
+				delete words;
+			}
+			i++;
+		}
 
-        saveObject(tokenCounter, path);
-        return tokenCounter;
-    } else {
-        LOG("Loading Data from ", path);
-        infile.close();
+		saveObject(tokenCounter, path);
+		return tokenCounter;
+	} else {
+		LOG("Loading Data from ", path);
+		infile.close();
 
-        return loadObject<Counter<string>>(path);
-    }
+		return loadObject < Counter < string >> (path);
+	}
 }
 
 double collectAUCP(unordered_map<long, User> *users) {
-    double aucp = 0.0;
-    for (auto &kv : *users) {
-        int cut = 0;
-        for (auto &t : kv.second.getTweets()) {
-            if (t.containsUrl()) {
-                cut++;
-            }
-        }
-        aucp += 1.0 * cut / kv.second.getTweets().size();
-    }
+	double aucp = 0.0;
+	for (auto &kv : *users) {
+		int cut = 0;
+		for (auto &t : kv.second.getTweets()) {
+			if (t.containsUrl()) {
+				cut++;
+			}
+		}
+		aucp += 1.0 * cut / kv.second.getTweets().size();
+	}
 
-    aucp /= users->size();
+	aucp /= users->size();
 
-    return aucp;
+	return aucp;
 }
 
 double collectARP(unordered_map<long, User> *users) {
-    double arp = 0.0;
-    for (auto &kv : *users) {
-        int r = 0;
-        for (auto &t : kv.second.getTweets()) {
-            if (t.isRetweet()) {
-                r++;
-            }
-        }
-        arp += 1.0 * r / kv.second.getTweets().size();
-    }
-    
-    arp /= users->size();
+	double arp = 0.0;
+	for (auto &kv : *users) {
+		int r = 0;
+		for (auto &t : kv.second.getTweets()) {
+			if (t.isRetweet()) {
+				r++;
+			}
+		}
+		arp += 1.0 * r / kv.second.getTweets().size();
+	}
 
-    return arp;
+	arp /= users->size();
+
+	return arp;
 }
 
 void countByLen(const Counter<string> *counter, const string &path) {
-    LOG("Counting Tokens by Length into ", path);
-    Counter<int> lenCounter;
-    counter->eachEntry([&](const string &key, const int &value) {
-        lenCounter.count(key.size(), value);
-    });
+	LOG("Counting Tokens by Length into ", path);
+	Counter<int> lenCounter;
+	counter->eachEntry([&](const string &key, const int &value) {
+		lenCounter.count(key.size(), value);
+	});
 
-    auto *top = lenCounter.getTop();
-    writeFile(path, [&](ofstream &out) {
-        for (auto &i : *top) {
-            out << i.first << "\t" << i.second << endl;
-        }
-    });
+	auto *top = lenCounter.getTop();
+	writeFile(path, [&](ofstream &out) {
+		for (auto &i : *top) {
+			out << i.first << "\t" << i.second << endl;
+		}
+	});
+}
+
+int countTweets(const unordered_map<long, User> *users) {
+	int count = 0;
+	for (auto & kv : *users) {
+		count += kv.second.getTweets().size();
+	}
+	return count;
 }
 
 void printDatasetStatistic() {
-    auto *spammers = loadSpammers();
-    auto *nonSpammers = loadSampledNonSpammers();
+	auto *spammers = loadSpammers();
+	auto *nonSpammers = loadSampledNonSpammers();
 
-    // average url contain percentage definition:
-    // aucp = user.containUrlTweets.size() / user.getTweets().size()
-    LOG("Counting Average URL Contains Percentage");
-    LOG_VAR(collectAUCP(spammers));
-    LOG_VAR(collectAUCP(nonSpammers));
+	LOG_VAR(spammers->size());
+	LOG_VAR(nonSpammers->size());
+	LOG_VAR(countTweets(spammers));
+	LOG_VAR(countTweets(nonSpammers));
 
-    // average retweet percentage definition:
-    // arp = user.retweets.size() / user.getTweets().size()
-    LOG("Counting Average Retweet Percentage");
-    LOG_VAR(collectARP(spammers));
-    LOG_VAR(collectARP(nonSpammers));
+	// average url contain percentage definition:
+	// aucp = user.containUrlTweets.size() / user.getTweets().size()
+	LOG("Counting Average URL Contains Percentage");
+	LOG_VAR(collectAUCP(spammers));
+	LOG_VAR(collectAUCP(nonSpammers));
 
-    LOG("Counting Tokens in Tweets from Spammers");
-    auto *spammerTokenCounter = countTokens(spammers, SPAMMER_TOKEN_COUNTER);
+	// average retweet percentage definition:
+	// arp = user.retweets.size() / user.getTweets().size()
+	LOG("Counting Average Retweet Percentage");
+	LOG_VAR(collectARP(spammers));
+	LOG_VAR(collectARP(nonSpammers));
 
-    LOG("Counting Tokens in Tweets from NonSpammers");
-    auto *nonSpammerTokenCounter =
-        countTokens(nonSpammers, NON_SPAMMER_TOKEN_COUNTER);
+	LOG("Counting Tokens in Tweets from Spammers");
+	auto *spammerTokenCounter = countTokens(spammers, SPAMMER_TOKEN_COUNTER);
 
-    LOG("Counting All Tokens in Tweets");
-    auto *allTokenCounter = countTokens(spammers, SPAMMER_TOKEN_COUNTER);
-    allTokenCounter->addCounter(*nonSpammerTokenCounter);
+	LOG("Counting Tokens in Tweets from NonSpammers");
+	auto *nonSpammerTokenCounter = countTokens(nonSpammers,
+			NON_SPAMMER_TOKEN_COUNTER);
 
-    delete spammers;
-    delete nonSpammers;
+	LOG("Counting All Tokens in Tweets");
+	auto *allTokenCounter = countTokens(spammers, SPAMMER_TOKEN_COUNTER);
+	allTokenCounter->addCounter(*nonSpammerTokenCounter);
 
-    countByLen(spammerTokenCounter, PATH + "spammer-len-dist.txt");
-    countByLen(nonSpammerTokenCounter, PATH + "non-spammer-len-dist.txt");
-    countByLen(allTokenCounter, PATH + "all-len-dist.txt");
+	delete spammers;
+	delete nonSpammers;
 
-    auto *spammerTokens = spammerTokenCounter->getKeySet();
-    auto *nonSpammerTokens = nonSpammerTokenCounter->getKeySet();
-    auto *sharedTokens = setIntersection(spammerTokens, nonSpammerTokens);
-    auto *allTokens = setUnion(spammerTokens, nonSpammerTokens);
+	countByLen(spammerTokenCounter, PATH + "spammer-len-dist.txt");
+	countByLen(nonSpammerTokenCounter, PATH + "non-spammer-len-dist.txt");
+	countByLen(allTokenCounter, PATH + "all-len-dist.txt");
 
-    LOG("Before removing stops");
-    LOG_VAR(spammerTokens->size());
-    LOG_VAR(nonSpammerTokens->size());
-    LOG_VAR(sharedTokens->size());
-    LOG_VAR(allTokens->size());
+	auto *spammerTokens = spammerTokenCounter->getKeySet();
+	auto *nonSpammerTokens = nonSpammerTokenCounter->getKeySet();
+	auto *sharedTokens = setIntersection(spammerTokens, nonSpammerTokens);
+	auto *allTokens = setUnion(spammerTokens, nonSpammerTokens);
 
-    delete spammerTokens;
-    delete nonSpammerTokens;
-    delete sharedTokens;
-    delete allTokens;
+	LOG("Before removing stops");
+	LOG_VAR(spammerTokens->size());
+	LOG_VAR(nonSpammerTokens->size());
+	LOG_VAR(sharedTokens->size());
+	LOG_VAR(allTokens->size());
 
-    spammerTokenCounter->saveFrequency(SPAMMER_TOKEN_FREQ);
-    nonSpammerTokenCounter->saveFrequency(NON_SPAMMER_TOKEN_FREQ);
-    allTokenCounter->saveFrequency(ALL_TOKEN_FREQ);
+	delete spammerTokens;
+	delete nonSpammerTokens;
+	delete sharedTokens;
+	delete allTokens;
 
-    LOG("After removing stops");
-    auto *stops = loadStops();
-    for (auto &k : *stops) {
-        spammerTokenCounter->deleteKey(k);
-        nonSpammerTokenCounter->deleteKey(k);
-        allTokenCounter->deleteKey(k);
-    }
-    
-    countByLen(spammerTokenCounter, PATH + "spammer-no-stops-len-dist.txt");
-    countByLen(nonSpammerTokenCounter, PATH + "non-spammer-no-stops-len-dist.txt");
-    countByLen(allTokenCounter, PATH + "all-no-stops-len-dist.txt");
+	spammerTokenCounter->saveFrequency(SPAMMER_TOKEN_FREQ);
+	nonSpammerTokenCounter->saveFrequency(NON_SPAMMER_TOKEN_FREQ);
+	allTokenCounter->saveFrequency(ALL_TOKEN_FREQ);
 
-    spammerTokens = spammerTokenCounter->getKeySet();
-    nonSpammerTokens = nonSpammerTokenCounter->getKeySet();
-    sharedTokens = setIntersection(spammerTokens, nonSpammerTokens);
-    allTokens = setUnion(spammerTokens, nonSpammerTokens);
+	LOG("After removing stops");
+	auto *stops = loadStops();
+	for (auto &k : *stops) {
+		spammerTokenCounter->deleteKey(k);
+		nonSpammerTokenCounter->deleteKey(k);
+		allTokenCounter->deleteKey(k);
+	}
 
-    LOG_VAR(spammerTokens->size());
-    LOG_VAR(nonSpammerTokens->size());
-    LOG_VAR(sharedTokens->size());
-    LOG_VAR(allTokens->size());
+	countByLen(spammerTokenCounter, PATH + "spammer-no-stops-len-dist.txt");
+	countByLen(nonSpammerTokenCounter,
+			PATH + "non-spammer-no-stops-len-dist.txt");
+	countByLen(allTokenCounter, PATH + "all-no-stops-len-dist.txt");
 
-    delete stops;
+	spammerTokens = spammerTokenCounter->getKeySet();
+	nonSpammerTokens = nonSpammerTokenCounter->getKeySet();
+	sharedTokens = setIntersection(spammerTokens, nonSpammerTokens);
+	allTokens = setUnion(spammerTokens, nonSpammerTokens);
 
-    delete spammerTokenCounter;
-    delete nonSpammerTokenCounter;
-    delete allTokenCounter;
+	LOG_VAR(spammerTokens->size());
+	LOG_VAR(nonSpammerTokens->size());
+	LOG_VAR(sharedTokens->size());
+	LOG_VAR(allTokens->size());
+
+	delete stops;
+
+	delete spammerTokenCounter;
+	delete nonSpammerTokenCounter;
+	delete allTokenCounter;
 }
 
 void testClassification() {
-    auto *spammerDS = Dataset::loadDataset(SPAMMER_DS, SPAMMER_VALUE);
-    auto *nonSpammerDS =
-        Dataset::loadDataset(NON_SPAMMER_DS, NON_SPAMMER_VALUE);
+	auto *spammerDS = Dataset::loadDataset(SPAMMER_DS, SPAMMER_VALUE);
+	auto *nonSpammerDS = Dataset::loadDataset(NON_SPAMMER_DS,
+			NON_SPAMMER_VALUE);
 
-    Classifier *cls = new NaiveBayes();
-    Evaluator eval;
+	Classifier *cls = new NaiveBayes();
+	Evaluator eval;
 
-    eval.crossValidate(10, cls, spammerDS, nonSpammerDS);
-    for (auto &item : eval.getConfusionMatrixVector()) {
-        LOG(item);
-    }
-    LOG_VAR(eval.getAccuracy());
-    LOG_VAR(eval.getRecall());
-    LOG_VAR(eval.getPrecision());
-    LOG_VAR(eval.getF1());
+	eval.crossValidate(10, cls, spammerDS, nonSpammerDS);
+	for (auto &item : eval.getConfusionMatrixVector()) {
+		LOG(item);
+	}
+	LOG_VAR(eval.getAccuracy());
+	LOG_VAR(eval.getRecall());
+	LOG_VAR(eval.getPrecision());
+	LOG_VAR(eval.getF1());
 
-    delete cls;
-    delete spammerDS;
-    delete nonSpammerDS;
+	delete cls;
+	delete spammerDS;
+	delete nonSpammerDS;
 }
 
 void testFeatureSelection() {
-    auto *spammerDS = Dataset::loadDataset(SPAMMER_DS, SPAMMER_VALUE);
-    auto *nonSpammerDS =
-        Dataset::loadDataset(NON_SPAMMER_DS, NON_SPAMMER_VALUE);
+	auto *spammerDS = Dataset::loadDataset(SPAMMER_DS, SPAMMER_VALUE);
+	auto *nonSpammerDS = Dataset::loadDataset(NON_SPAMMER_DS,
+			NON_SPAMMER_VALUE);
 
-    auto *all = spammerDS;
-    all->addDataset(*nonSpammerDS);
-    delete nonSpammerDS;
+	auto *all = spammerDS;
+	all->addDataset(*nonSpammerDS);
+	delete nonSpammerDS;
 
-    auto *selector = new BiClassMutualInformation();
-    selector->train(all);
+	auto *selector = new BiClassMutualInformation();
+	selector->train(all);
 
-    vector<pair<string, double>> *result = selector->getTopFeatureList();
-    writeFile(PATH + "feature.txt", [&](ofstream &out) {
-        for (auto &r : *result) {
-            out << r.first << "\t" << r.second << endl;
-        }
-    });
+	vector<pair<string, double>> *result = selector->getTopFeatureList();
+	writeFile(PATH + "feature.txt", [&](ofstream &out) {
+		for (auto &r : *result) {
+			out << r.first << "\t" << r.second << endl;
+		}
+	});
 
-    delete selector;
-    delete all;
+	delete selector;
+	delete all;
 }
 
 int main(int argc, char const *argv[]) {
-    printDatasetStatistic();
-    return 0;
+	printDatasetStatistic();
+	return 0;
 }
--- src/data.cc
+++ src/data.cc
@@ -1,37 +1,39 @@
 #include "data.h"
 
 ostream &operator<<(ostream &out, Tweet &t) {
-    out << "Tweet[" << t.getText() << "]";
-    return out;
+	out << "Tweet[" << t.getText() << "]";
+	return out;
 }
 
-static const string RT_PREFIX = "RT @";
+static const string RT_PREFIX = "RT";
+static const string LOWER_RT_PREFIX = "rt";
 
 bool Tweet::isRetweet() const { // see
-    // https://dev.twitter.com/overview/api/entities-in-twitter-objects
-    return !text.compare(0, RT_PREFIX.size(), RT_PREFIX);
+	// https://dev.twitter.com/overview/api/entities-in-twitter-objects
+	return !(text.compare(0, RT_PREFIX.size(), RT_PREFIX)
+			|| text.compare(0, LOWER_RT_PREFIX.size(), LOWER_RT_PREFIX));
 }
 
 static const regex URL_MATCHER(".*((http)|(https))://(\\w+|\\.|/)+.*");
 
 bool Tweet::containsUrl() const {
-    return regex_match(text.begin(), text.end(), URL_MATCHER);
+	return regex_match(text.begin(), text.end(), URL_MATCHER);
 }
 
 void User::loadTweets() {
-    string filename =
-        (spammer ? SPAMMER_TWEET_PATH : NON_SPAMMER_TWEET_PATH) + to_string(id);
+	string filename = (spammer ? SPAMMER_TWEET_PATH : NON_SPAMMER_TWEET_PATH)
+			+ to_string(id);
 
-    readFile(filename, false, [&](int index, string &line) {
-        tweets.push_back(Tweet(line));
-        return true;
-    });
+	readFile(filename, false, [&](int index, string &line) {
+		tweets.push_back(Tweet(line));
+		return true;
+	});
 }
 
 ostream &operator<<(ostream &out, User &u) {
-    out << "User[" << u.getId() << ", Tweets Count = " << u.getTweets().size()
-        << "]";
-    return out;
+	out << "User[" << u.getId() << ", Tweets Count = " << u.getTweets().size()
+			<< "]";
+	return out;
 }
 
 /*
@@ -40,110 +42,110 @@ ostream &operator<<(ostream &out, User &u) {
 
 unordered_map<long, User> *
 loadData(const string &dataPath, const string &path, const bool spammer,
-         function<void(unordered_map<long, User> *)> postDataHandler) {
-    ifstream infile(dataPath);
+		function<void(unordered_map<long, User> *)> postDataHandler) {
+	ifstream infile(dataPath);
 
-    if (!infile.good()) {
-        LOG("Cannot Find ", dataPath, ". Loading File from ", path);
-        infile.close();
+	if (!infile.good()) {
+		LOG("Cannot Find ", dataPath, ". Loading File from ", path);
+		infile.close();
 
-        unordered_map<long, User> *users = new unordered_map<long, User>();
-        readFile(path, true, [&](int index, string &line) {
-            long id = stol(line);
-            User u(id, spammer);
-            u.loadTweets();
-            (*users)[id] = u;
-            return true;
-        });
+		unordered_map<long, User> *users = new unordered_map<long, User>();
+		readFile(path, true, [&](int index, string &line) {
+			long id = stol(line);
+			User u(id, spammer);
+			u.loadTweets();
+			(*users)[id] = u;
+			return true;
+		});
 
-        postDataHandler(users);
-        saveObject(users, dataPath);
+		postDataHandler(users);
+		saveObject(users, dataPath);
 
-        return users;
-    } else {
-        LOG("Loading Data from ", dataPath);
-        infile.close();
+		return users;
+	} else {
+		LOG("Loading Data from ", dataPath);
+		infile.close();
 
-        return loadObject<unordered_map<long, User>>(dataPath);
-    }
+		return loadObject<unordered_map<long, User>>(dataPath);
+	}
 }
 
 unordered_map<long, User> *loadData(const string &dataPath, const string &path,
-                                    const bool spammer) {
-    return loadData(dataPath, path, spammer,
-                    [](unordered_map<long, User> *users) {});
+		const bool spammer) {
+	return loadData(dataPath, path, spammer,
+			[](unordered_map<long, User> *users) {});
 }
 
 unordered_map<long, User> *loadSpammers() {
-    return loadData(SPAMMER_DATA_PATH, SPAMMER_ID_LIST, true);
+	return loadData(SPAMMER_DATA_PATH, SPAMMER_ID_LIST, true);
 }
 
 unordered_map<long, User> *loadSampledNonSpammers() {
-    return loadData(NON_SPAMMER_DATA_PATH, SAMPLED_NON_SPAMMER_ID_LIST, false,
-                    [](unordered_map<long, User> *users) {
-        for (auto &kv : *users) {
-            random_shuffle(kv.second.getTweets().begin(),
-                           kv.second.getTweets().end());
-            kv.second.getTweets().erase(kv.second.getTweets().begin() +
-                                            SAMPLE_TWEET_SIZE,
-                                        kv.second.getTweets().end());
-        }
-    });
+	return loadData(NON_SPAMMER_DATA_PATH, SAMPLED_NON_SPAMMER_ID_LIST, false,
+			[](unordered_map<long, User> *users) {
+				for (auto &kv : *users) {
+					random_shuffle(kv.second.getTweets().begin(),
+							kv.second.getTweets().end());
+					kv.second.getTweets().erase(kv.second.getTweets().begin() +
+							SAMPLE_TWEET_SIZE,
+							kv.second.getTweets().end());
+				}
+			});
 }
 
 void sampleNonSpammers() {
-    LOG("Start Sampling Normal Users...");
-    unordered_set<long> spammerIds;
-    readFile(SPAMMER_ID_LIST, [&](int i, string &line) {
-        spammerIds.insert(stol(line));
-        return true;
-    });
-    LOG_VAR(spammerIds.size());
+	LOG("Start Sampling Normal Users...");
+	unordered_set<long> spammerIds;
+	readFile(SPAMMER_ID_LIST, [&](int i, string &line) {
+		spammerIds.insert(stol(line));
+		return true;
+	});
+	LOG_VAR(spammerIds.size());
 
-    vector<long> nonSpammerIds;
-    readFile(NON_SPAMMER_ID_LIST, [&](int i, string &line) {
-        long id = stol(line);
-        if (spammerIds.find(id) == spammerIds.end()) {
-            User u(id, false);
-            u.loadTweets();
-            if (u.getTweets().size() >= SAMPLE_TWEET_SIZE) {
-                nonSpammerIds.push_back(id);
-            }
-        }
-        if (nonSpammerIds.size() % 100 == 0 && nonSpammerIds.size() != 0) {
-            LOG("Sampled ", nonSpammerIds.size(), " users");
-        }
-        return true;
-    });
-    LOG_VAR(nonSpammerIds.size());
+	vector<long> nonSpammerIds;
+	readFile(NON_SPAMMER_ID_LIST, [&](int i, string &line) {
+		long id = stol(line);
+		if (spammerIds.find(id) == spammerIds.end()) {
+			User u(id, false);
+			u.loadTweets();
+			if (u.getTweets().size() >= SAMPLE_TWEET_SIZE) {
+				nonSpammerIds.push_back(id);
+			}
+		}
+		if (nonSpammerIds.size() % 100 == 0 && nonSpammerIds.size() != 0) {
+			LOG("Sampled ", nonSpammerIds.size(), " users");
+		}
+		return true;
+	});
+	LOG_VAR(nonSpammerIds.size());
 
-    random_shuffle(nonSpammerIds.begin(), nonSpammerIds.end());
+	random_shuffle(nonSpammerIds.begin(), nonSpammerIds.end());
 
-    writeFile(SAMPLED_NON_SPAMMER_ID_LIST, [&](ofstream &out) {
-        for (int i = 0; i < spammerIds.size(); i++) {
-            out << nonSpammerIds[i] << endl;
-        }
-    });
+	writeFile(SAMPLED_NON_SPAMMER_ID_LIST, [&](ofstream &out) {
+		for (int i = 0; i < spammerIds.size(); i++) {
+			out << nonSpammerIds[i] << endl;
+		}
+	});
 }
 
 Dataset *user2Dataset(unordered_map<long, User> *users, int gramLen) {
-    Dataset *dataset = new Dataset();
+	Dataset *dataset = new Dataset();
 
-    for (auto &kv : *users) {
-        Instance ins;
-        ins.setClassValue(kv.second.isSpammer() ? SPAMMER_VALUE
-                                                : NON_SPAMMER_VALUE);
+	for (auto &kv : *users) {
+		Instance ins;
+		ins.setClassValue(
+				kv.second.isSpammer() ? SPAMMER_VALUE : NON_SPAMMER_VALUE);
 
-        for (auto &t : kv.second.getTweets()) {
-            vector<string> *grams = toGrams(t.getText(), gramLen);
-            for (auto &g : *grams) {
-                ins[g] += 1.0;
-            };
-            delete grams;
-        };
+		for (auto &t : kv.second.getTweets()) {
+			vector < string > *grams = toGrams(t.getText(), gramLen);
+			for (auto &g : *grams) {
+				ins[g] += 1.0;
+			};
+			delete grams;
+		};
 
-        dataset->addInstance(ins);
-    };
+		dataset->addInstance(ins);
+	};
 
-    return dataset;
+	return dataset;
 }
